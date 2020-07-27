af967ce839f176dc4bfadf0ab0493fa73918414c
Hannes <7892467+hannes-dev@users.noreply.github.com>
2020-07-27 19:51:35 +0200

    start basic parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::process::Command;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    struct File {
+        path: String,
+        selected: bool,
+    }
+
+    parse_status();
+}
+
+fn parse_status() {
+    let result = Command::new("git")
+            .arg("status")
+            .arg("--porcelain")
+            .output()
+            .expect("failed to run git status");
+    
+    for item in result.stdout.split(|num| num == &10) {
+        let info = item.split_at(2);
+        
+        println!("{:?}", item);
+    }
 }
