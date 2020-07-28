7607cdec28af920a30f8299d04cda950c6daa174
Wout Schellaert <wout.schellaert@gmail.com>
2020-07-28 11:54:36 +0200

    Move empty output check outside of loop and use built-ins
    It's not necessary to recheck the output every iteration, moving this
    condition to the front also makes it more explicit what your return in
    that case is.
    We also use the built-in trim and is_empty to make it clear there can
    only be whitespace.

--- src/main.rs
+++ src/main.rs
@@ -29,24 +29,26 @@ fn parse_status() -> Vec<File> {
         .output()
         .expect("failed to run git status");
 
-    let mut files = Vec::new();
+    let output: &str = std::str::from_utf8(&result.stdout).expect("Invalid UTF in git output");
+
+    // Empty output, no changes
+    if output.trim_start().is_empty() {
+        return vec![];
+    }
 
     // split command output on newlines
-    let output: &str = std::str::from_utf8(&result.stdout).expect("Invalid UTF in git output");
+    let mut files = Vec::new();
     for item in output.split('\n') {
-        // if it's not just an empty line
-        if item.len() >= 2 {
-            let (status, path) = item.split_at(3);
-            let added = !status.starts_with(' ')
-                && !status.starts_with('?')
-                && status.chars().nth(1).unwrap() != 'M';
+        let (status, path) = item.split_at(3);
+        let added = !status.starts_with(' ')
+            && !status.starts_with('?')
+            && status.chars().nth(1).unwrap() != 'M';
 
-            files.push(File {
-                path: path.to_owned(),
-                added,
-                to_add: added,
-            });
-        }
+        files.push(File {
+            path: path.to_owned(),
+            added,
+            to_add: added,
+        });
     }
 
     files
